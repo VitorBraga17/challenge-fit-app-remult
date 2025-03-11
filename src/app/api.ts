824be8c7863654f6edcfadafed4f331ed5433c35
vitorbraga17 <joaovitor1702@gmail.com>
2025-03-11 11:04:34 -0300

    corrigido primeiro commit

--- src/app/api.ts
+++
@@ -1,5 +0,0 @@
-// src/api.ts
-
-import { remultNextApp } from 'remult/remult-next'
-
-export const api = remultNextApp({})
--- src/app/remult/route.ts
+++
@@ -1,5 +0,0 @@
-// src/app/api/[...remult]/route.ts
-
-import { api } from '../api'
-
-export const { POST, PUT, DELETE, GET } = api
